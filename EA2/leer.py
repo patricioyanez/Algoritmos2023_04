5c54a0306b75bd28fa168e578db68cc37023bea3
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-04-04 11:17:50 -0400

    update

---
+++ EA2/leer.py
@@ -0,0 +1,6 @@
+print("Ingrese su nombre:")
+nombre = input() # input() es igual al leer en pseInt
+print("Hola ", nombre)
+
+apellido = input("Ingrese su apellido:")
+print("Su nombre completo es:", nombre, apellido)
