76ec634da04feab348a9c1a0ef73b42a5a4127be
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-04-11 13:23:36 -0400

    Update EjercicioIfElifElse.py

--- EA2/EjercicioIfElifElse.py
+++ EA2/EjercicioIfElifElse.py
@@ -1,2 +1,15 @@
 # Solicitar edad y clasifiquelo según rango etario
 # niño, adolescente, adulto, adulto mayor
+
+print("|=== Clasificación etaria ===|")
+edad = input("Ingrese su edad:") # str (string)
+edad = int(edad)  # convierte el str en int
+
+if edad <= 12:
+    print("Ud es un niño")
+elif edad < 19:
+    print("Ud es un adolescente")
+elif edad < 65:
+    print("Ud es un adulto")
+else:
+    print("Ud es adulto mayor")
