0bc4f6d989129ec7122db803313633f38637b457
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-04-17 12:41:49 -0400

    update

--- EA2/Ejercicio4ConvertidorAPeso.py
+++ EA2/Ejercicio4ConvertidorAPeso.py
@@ -5,3 +5,21 @@
 # Yen a pesos chilenos 
 # 
 # Buscar los valores de hoy
+
+print("*** Convertior a Pesos Chileno ***")
+usd = 801
+arg = .27
+yen = .16
+valorMoneda = 0.0
+monto = float(input("Ingrese monto de la moneda:"))
+moneda= input("Seleccione moneda [1]USD [2] ARG [3]Yen:")
+
+if moneda == "1":
+    valorMoneda = usd
+elif moneda == "2":
+    valorMoneda = arg
+else:
+    valorMoneda = yen
+
+total = valorMoneda * monto
+print("El total en pesos CL es: $", int(total))
