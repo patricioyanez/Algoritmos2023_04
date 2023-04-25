74128d3776e4222973d518bb5091fb5abde77d22
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-04-25 12:00:52 -0400

    update

--- EA2/EjercicioFor1.py
+++ EA2/EjercicioFor1.py
@@ -2,3 +2,24 @@
 # Cuántos números son mayores a cero
 # Cuántos números son menores a cero
 # Cuántos números son iguales a cero
+
+mayoresACero = 0
+menoresACero = 0
+igualesACero = 0
+
+rango = range(5)
+
+for i in rango:
+    numero = int(input(f"ingrese número {(i+1)}:"))
+
+    if numero > 0:
+        mayoresACero += 1
+    elif numero < 0:
+        menoresACero += 1
+    else:
+        igualesACero += 1
+
+#mostrar resultados
+print("Cantidad de mayores a cero: ", mayoresACero)
+print("Cantidad de menores a cero: ", menoresACero)
+print("Cantidad de iguales a cero: ", igualesACero)
---
+++ EA2/EjercicioFor2.py
@@ -0,0 +1,20 @@
+# Ingrese por teclado 10 letras, 
+# indique cuantas de ellas son vocales y 
+# cuántas son consonantes.
+
+rango = range(10)
+vocales = 0
+consonantes= 0
+for x in rango:
+    letra = input("Ingrese una letra:")
+
+    #if letra == "a" or letra == "e"....
+
+    if letra.lower() in ("a", "e", "i", "o", "u"):
+        vocales +=1
+    else:
+        consonantes += 1
+
+# resultados
+print("Vocales      : ", vocales)
+print("Consonantes  : ", consonantes) 
