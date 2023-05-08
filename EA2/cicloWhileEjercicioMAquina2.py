f7c6ab6d355287a4ac048b8425a7e1b36c17b026
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-05-08 12:48:50 -0400

    update

--- EA2/cicloWhileEjercicioMAquina2.py
+++ EA2/cicloWhileEjercicioMAquina2.py
@@ -10,18 +10,39 @@
 # -	Si el dinero es mayor a $400 debe calcular y entregar vuelto.
 # -	Si el dinero es menor a $400 debe mandar un mensaje de alerta y no entregar la bebida.
 # Finalizado el proceso, esperar una nueva orden.
+
+## agregar contadores para saber cuantas bebidas se vendieron de cada una. 
+# Crear otra opcion en el menú
+
+
+# Agregar opción total de ventas: Suma todas las ventas realizadas. Ocupar un acumulador
+
 opcion = ""
+cocacola = 0
+fanta = 0
+sprite = 0
+
+fueVendida = False
+
 
 while opcion != "4":
     print("1.- Coca-Cola")
     print("2.- Fanta")
     print("3.- Sprite")
-    print("4.- Salir")
+    print("4.- Contadores")
+    print("5.- Salir")
 
     opcion = input("Ingrese una opción:")
 
-    if opcion not in ("1", "2", "3", "4"):
+    if opcion not in ("1", "2", "3", "4", "5"):
         print("La opción no es válida")
+    if opcion == "4":
+        print("**** R E P O R T E ****")
+        print("CocaCola : ", cocacola)
+        print("Fanta    : ", fanta)
+        print("Sprite   : ", sprite)
+        print("Total de bebidas: ", (cocacola+ fanta + sprite))
+
     else:
 
         monto = int(input("Ingrese pago:"))
@@ -30,8 +51,18 @@ while opcion != "4":
 
         if vuelto == 0:
             print("Gracias por su compra")
+            fueVendida = True
         elif vuelto > 0:
             print("Gracias por su compra, su vuelto es $" + str(vuelto))
+            fueVendida = True
         else:
             print("Falta dinero, vuelva a intentar")
-
+            fueVendida = False
+        
+        if fueVendida:
+            if opcion == "1":
+                cocacola += 1
+            elif opcion == "2":
+                fanta += 1
+            else:
+                sprite += 1
