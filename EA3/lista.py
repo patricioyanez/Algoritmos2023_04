6c195a17dd64374429165cddbd15409df36a214c
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-05-22 12:32:34 -0400

    update

---
+++ EA3/lista.py
@@ -0,0 +1,16 @@
+# listas
+
+lista = [100, "a", True, 2.5]
+
+print(lista)
+
+print("Valor del indice 1:", lista[1])
+print("Valor del indice 3:", lista[3])
+#indice   0   1  2  3
+lista2 = [10,20,30,40]
+#indece   -4 -3 -2 -1 
+print("El valor del indice 1:", lista2[1])
+print("La cantidad de elementos son:", len(lista2))
+print("El valor del indice -1:", lista2[-1]) # es el último elemento
+print("El valor del indice -2:", lista2[-2]) # es el último elemento
+print(lista2)
