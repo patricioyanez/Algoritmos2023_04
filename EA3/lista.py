29a8d0688ee217d356560c5dc8a2336bbd38d034
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-05-23 10:45:25 -0400

    upfate

--- EA3/lista.py
+++ EA3/lista.py
@@ -14,3 +14,24 @@ print("La cantidad de elementos son:", len(lista2))
 print("El valor del indice -1:", lista2[-1]) # es el último elemento
 print("El valor del indice -2:", lista2[-2]) # es el último elemento
 print(lista2)
+lista2.append(1000)
+print(lista2)
+
+print("La cantidad de elementos son:", len(lista2))
+
+# pip install numpy
+# black box
+lista2.reverse() # invierte los valores de un arreglo
+print(lista2)
+
+
+# agregar elemento en indice diferente
+
+lista2.insert(3, 5000)
+print(lista2)
+
+lista3 = ["a", "b"]
+# une 2 listas en una sola
+lista2.extend(lista3)
+print(lista2)
+print(lista3)
