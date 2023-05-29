281d4c3c9ae047c998f1f75542b8f2fe1a6bc0e4
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-05-29 12:34:26 -0400

    update

--- EA3/arreglo.py
+++ EA3/arreglo.py
@@ -39,3 +39,41 @@ resultado = arr1 * arr2
 print(resultado)
 resultado = arr1 / arr2
 print(resultado)
+
+print(arr1 == arr2)
+print(arr1 > arr2) # !=, >=,< o <=
+
+print("Total de la suma de los elementos: ", arr1.sum())
+print("promedio de los elementos: ", arr1.mean())
+print("Valor menor de los elementos: ", arr1.min())
+print("Valor mayor de los elementos: ", arr1.max())
+
+
+#copia / igualdad
+
+arreglo = np.array([1,5,7,3,6,10,55,45,11,77])
+print(arreglo)
+arreglo_copia = arreglo
+print(arreglo_copia)
+arreglo_copia[0] = 99
+print(arreglo)
+
+arreglo3 = arreglo.copy()
+print(arreglo3)
+arreglo3[0] = 500
+print(arreglo)
+print(arreglo3)
+
+#Ejercicios:
+
+# 1.1 Crear un arreglo unidimensional de tamaño 10, 
+# con elementos aleatorios de números enteros del 0 al 100, 
+# para ello deberá investigar la función que 
+# permita crear números aleatorios.
+# 
+# 1.2. Crear una copia del arreglo y muestre:
+# Elemento mayor
+# Elemento menor
+# Suma de los elementos
+# Promedio de los elementos
+# Mostrar todos los elementos
