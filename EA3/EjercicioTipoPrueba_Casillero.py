a1805d1e6e1a3dd7ead27c09e29b3a5a058340f0
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-06-12 12:12:40 -0400

    Update

--- EA3/EjercicioTipoPrueba_Casillero.py
+++ EA3/EjercicioTipoPrueba_Casillero.py
@@ -19,3 +19,57 @@
 
 # Usar funciones para cada una de las opciones
 # validar que no esten vacios los datos a ingresar
+
+import numpy as np
+import os
+
+# declarar variables
+casilleros = np.array([ ["","","","",""],
+                        ["","","","",""],
+                        ["","","","",""],
+                       ], dtype=object)
+
+totalVentas = 0 # acumulador. Para sumar todas las ventas realizadas
+filaCasillero = -1 # almacenará el tipo de casillero
+columnaCasillero = -1 # almacenará el nro del casillero
+
+listaDeOpcionesValidas = ["0","1","2","3","4"]
+listaDeTiposCasilleros = ["1","2","3"]
+opcion = ""
+# declara las funciones para cada opción:
+def registroDeArriendo(casilleros):
+    pass
+def listarCasilleros(casilleros):
+    pass
+def totalDeVentas(casilleros):
+    pass
+def desocuparCasillero(casilleros):
+    pass
+
+# definir el menú
+while opcion != "0":
+    os.system("cls")
+    print("======= Administración de Casilleros ========")
+    print("1.- Registro de arriendo")
+    print("2.- Listar casilleros (ocupados y los que no)")
+    print("3.- Total de ventas")
+    print("4.- Desocupar casillero")
+    print("0.- Salir")
+    opcion = input("Seleccione una opción: ")
+
+    #validar opción seleccionada
+    if opcion not in listaDeOpcionesValidas:
+        print("La opción ingresada no es válida")
+        input("Presione enter para continuar...")
+    elif opcion == "1":
+        registroDeArriendo(casilleros)
+    elif opcion == "2":
+        listarCasilleros(casilleros)
+    elif opcion == "3":
+        totalDeVentas(casilleros)
+    elif opcion == "4":
+        desocuparCasillero(casilleros)
+
+
+
+
